91115fda8f57cc1a5618c1841342b5d8fbe3b48c
Evgeniy <148543433+Evgeniy0y0@users.noreply.github.com>
2024-10-15 14:44:12 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+def createfile(filename):
+  try:
+    file = open(filename, "w")
+    print("File successfully created")
+  except:
+    print("Failed to create file")
+      
+def writefile(filename, words):
+  try:
+    file = open(filename, "w")
+    file.write(text)
+    print("File successfully written.")
+  except:
+    print("Failed to write to file")
+
+text = "How`s your day going?"
+try:
+  createfile("Text.txt")
+  print("File Text.txt successfully created.")
+  writefile("Text.txt", text)
+  try:
+    file = open("Text.txt", "r")
+    print("Text.txt: ")
+    print(file.read())
+  except:
+     print("Failed to open file")
+except:
+  print("Failed to create file")
